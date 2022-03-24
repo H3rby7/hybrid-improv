058b02dce9eb66dbf32c5f9eaad786bb3678490b
h3rby7 <herbielutz@gmx.net>
2022-03-24 21:31:49 +0100

    controllable projection angle

--- options.js
+++ options.js
@@ -1,11 +1,15 @@
 (function() {
   const prj = document.querySelector("#projection");
+  let prjSide = 'right';
+
   function changeProjectionSide(val) {
     prj.classList.remove("left", "right");
+    prjSide = val;
     prj.classList.add(val);
+    changeProjectionAngle(document.querySelector("#prjDeg").value);
 
     const hint = document.querySelector("#prjLocHint");
-    if (val === 'right') {
+    if (isRight()) {
       hint.innerHTML = "place your own camera left of this screen<br>&smt;---------"
     } else {
       hint.innerHTML = "place your own camera right of this screen<br>---------&gt;"
@@ -23,4 +27,17 @@
     prj.style.top = val + "px";
   }
   changeProjectionDistance(document.querySelector("#prjDist").value);
+
+  document.querySelector("#prjDeg").addEventListener('input', ({target: {value: val}}) => {
+    changeProjectionAngle(val);
+  }) 
+  function changeProjectionAngle(val) {
+    const orientation = isRight() ? "-" : ""
+    prj.style.transform = `rotateX(-90deg) rotateY(${orientation}${val}deg)`;
+  }
+  changeProjectionAngle(document.querySelector("#prjDeg").value);
+
+  function isRight() {
+    return prjSide === 'right';
+  }
 })()
